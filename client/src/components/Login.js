037ac37d15833d7acdd5eec7907667dda34c4e6a
Khoa Nguyen <khoadangnguyen2012@gmail.com>
2020-04-14 17:43:09 +0700

    Upload to github

---
+++ client/src/components/Login.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {Button, TextField, Fab} from '@material-ui/core';
+import {Facebook, Mail} from '@material-ui/icons';
+import styles from '../styles/Login.css.js';
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div style={styles.app}>
+                <div style={styles.container}>
+                    <div style={styles.panelLeft}>
+                        <h1 style={styles.title}>Hallo, Leute</h1>
+                        <p>Wir sind REC !</p>
+                    </div>
+                    <div style={styles.panelRight}>
+                        <h1 style={{fontSize: '40px'}}>Sign in</h1>
+                        <div>
+                            <Fab style={styles.signinFacebook}color="primary">
+                                <Facebook/>
+                            </Fab>
+                            <Fab style={styles.signinMail} color="primary">
+                                <Mail/>
+                            </Fab>
+                        </div>
+                        <p style={styles.p}>or use your account</p>
+                        <TextField
+                            style={styles.loginField}
+                            variant="outlined"
+                            color="primary"
+                            label="Email"
+                        />
+                        <TextField
+                            style={styles.loginField}
+                            variant="outlined"
+                            color="primary"
+                            label="Password"
+                        />
+                        <p style={styles.p}>Forgot your password ?</p>
+                        <Button
+                            style={styles.loginBtn}
+                            variant="contained"
+                            color="primary"
+                        >
+                        Sign in
+                        </Button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Login;
---
+++ client/src/styles/Login.css.js
@@ -0,0 +1,72 @@
+const styles = {
+    app: {
+        margin: '0',
+        padding: '0',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItem: 'center',
+        height: '100vh',
+    },
+    textfield: {
+        marginTop: '5px',
+        width: '500px'
+    },
+    container: {
+        display: 'flex',
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        borderRadius: '10px',
+        boxShadow: '0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22)',
+        position: 'relative',
+        overflow: 'hidden',
+        width: '768px',
+        maxWidth: '100%',
+        minHeight: '480px'
+    },
+    panelLeft: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '500px',
+        width: '50%',
+        backgroundColor: 'orange',
+        justifyContent: 'center',
+        textAlign: 'center'
+    },
+    title: {
+        margin: '0'
+    },
+    panelRight: {
+        width: '50%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItem: 'center',
+        textAlign: 'center'
+    },
+    signinMail: {
+        margin: '5px',
+        backgroundColor: '#D44638'
+    },
+    signinFacebook: {
+        margin: '5px',
+        backgroundColor: '#3b5998'
+    },
+    loginBtn: {
+        width: '50%',
+        height: '50px',
+        marginTop: '15px',
+        alignSelf: 'center',
+        backgroundColor: 'orange'
+    },
+    loginField: {
+        width: '80%',
+        marginTop: '15px',
+        alignSelf: 'center'
+    },
+    p: {
+        margin: '8px',
+        fontSize: '15px',
+        color: 'grey'
+    }
+  }
+
+export default styles
